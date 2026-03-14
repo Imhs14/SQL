45ffeb480407f54592fb7be5125b4002dcf4ba05
imhs14 <heerashanker0214@gmail.com>
2026-03-14 22:46:11 +0530

    feat: Add SQL examples for SQL `SELECT`, `DISTINCT`, and `COUNT(DISTINCT)` operations.

---
+++ select-ops.sql
@@ -0,0 +1,6 @@
+SELECT * from bmw;
+-- * returns the all the coloums in the table bmw
+SELECT c1,c2,c3 from bmw;
+-- columns seperated by the comma returns that paritcular coloumns from table bmw
+SELECT year,region, month from bmw;
+-- columns seperated by the comma returns that paritcular coloumns from table bmw
