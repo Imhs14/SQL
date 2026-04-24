d7b3543557fda538af0bf2c5da055210025b60fa
imhs14 <heerashanker0214@gmail.com>
2026-04-25 00:23:46 +0530

    update

--- NOT.sql
+++ NOT.sql
@@ -1,27 +1,40 @@
---- Not in SQL
---- NOT operator is used in WHERE clause to return all records that do not match the specified criteria.
---- it reverses the result of the condition from true to False and vice-versa 
-Select * from customers
-where not country = 'spain';
---- In the example above, the NOT operator is used in combination with the = operator.
---- The NOT operator is also used in combination with other operators to exclude data, such as:
---- NOT LIKE
---- NOT BETWEEN
---- NOT IN
---- IS NOT NULL
---- NOT EXISTS
---- A percent sign % - represents zero, one, or multiple characters
---- A underscore sign _ - represents a single character
---- The following SQL selects all customers that do NOT start with the letter "A":
-select * FROM customers
-where name not like 'A%';
+--- The NOT operator is used in Where clause it returns 
+--- it will remove the specified criteria and will return the rest 
+--- basically it will make the TRUE to False and vise verca
+select *from customers
+where not country = 'USA'; --- in the country where there is USA in country that is removed 
 
+/* SELECT column1, column2, ...
+FROM table_name
+WHERE NOT condition; */
+
+--- Using NOT LIKE
+/* NOT LIKE used to exclude the row that matches the specified character pattern 
+there are 2 wild card conjunction used with not like operator 
+% represents Zero, one or multiple characters 
+_ and underscore sign represents a single character */
+
+select * from bmw
+where model not like 'X3%';
+
+/* using the NOT BETWEEN operator in WHERE clause
+To select rows where a value fails outside the specified inclusive range */
+--- it selects all the customer id except from 2-8 
 select * from customers
-where name not like 'B_';
- -- NOt BETWEEN operator 
- -- gives the id's that are not in between the specified range 
- SELECT * from customers
- where id not between 10 and 30 ;
- -- The NOT BETWEEN operator can be used with numeric, text, or date values. 
+where id not between 2 and 8;
 
--- NOT IN operator 
+--- using NOT IN 
+/* this will exclude the rows that match any value in a specified list or subquery result set */
+
+select *from bmw
+where region not in ('Europe', 'France');
+--- it will not select the region i.e in the list above 
+
+--- it will not select the year that is greater then 2020
+select * from bmw
+where not year > 2020 ;
+
+--- it will 
+select * from bmw
+where not year < 2023;
+
