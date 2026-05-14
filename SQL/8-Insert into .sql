ef7b8139489e77762ef5550c60cac28b6d3b218f
imhs14 <heerashanker0214@gmail.com>
2026-05-14 23:17:24 +0530

    insert into

--- SQL/8-Insert into .sql
+++ SQL/8-Insert into .sql
@@ -16,10 +16,22 @@ VALUES (value1, value2, value3)
  --- INSERT INTO myowncsv (sno,model, company, horsepower, rating)
  --- VALUES ('3','sf90','ferrari','100','7'); this is how we insert 1 row of data 
  --- now let us insert multiple rows of the data
- insert into myowncsv (sno, model, company , horsepower, rating)
+ --- for inserting multiple values 
+insert into myowncsv (sno, model, company , horsepower, rating)
  VALUES 
- ('4', 'bronco','ford', '300', '9'),
- ('5','civic','honda',200,'8'),
- ('6','5 serise','BMW','250','8'),
- ('7','M 340i','BMW',345,10);
+ (3,'Chiron', 'buggatit', 1600, 10),
+ (4, 'bronco','ford', '300', '9'),
+ (5,'civic','honda',200,'8'),
+ (6,'5 serise','BMW','250','8'),
+ (7,'M 340i','BMW',345,10);
+
+---  for inserting single row 
+insert into myowncsv( sno, model , company , horsepower , rating)
+values (8, 'valhala' , 'Aston Martin' , 1000, 10);
+
+---  hence just like that we will insert the items into the sql table 
+insert into myowncsv(model, company, horsepower, rating)
+values ( 'Laferrari' , ' ferrari' , 700 , 10); 
+---  here to auto increment we need to write code at when creating the table to specify to auto increment 
+
 select *from myowncsv;
