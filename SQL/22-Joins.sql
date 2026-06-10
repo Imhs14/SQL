c0d143b5f9384301cae716f121e84bb79767ad06
imhs14 <heerashanker0214@gmail.com>
2026-06-11 01:41:55 +0530

    update on joins

--- SQL/22-Joins.sql
+++ SQL/22-Joins.sql
@@ -1,8 +1,19 @@
-'''The SQL JOIN Clause 
+/* The SQL JOIN Clause 
 The JOIN clause is used to combine rows from two or more tables, based on a related column between them 
 Here are the different types of JOINs in SQL:
 
 (INNER) JOIN: Returns only rows that have matching values in both tables
 LEFT (OUTER) JOIN: Returns all rows from the left table, and only the matched rows from the right table
 RIGHT (OUTER) JOIN: Returns all rows from the right table, and only the matched rows from the left table
-FULL (OUTER) JOIN: Returns all rows when there is a match in either the left or right table     '''
+FULL (OUTER) JOIN: Returns all rows when there is a match in either the left or right table     
+
+
+Quick Summary Cheat Sheet
+INNER JOIN: Only the overlap (Matches only).
+
+LEFT JOIN: Everything on the left + matching right details.
+
+RIGHT JOIN: Everything on the right + matching left details.
+
+FULL JOIN: Absolutely everything from both sides.  */
+
