fec0c2e658ff8306830e184fea44b51c7e7123b9
imhs14 <heerashanker0214@gmail.com>
2026-06-12 23:52:02 +0530

    joins intro done

--- SQL/22-Joins.sql
+++ SQL/22-Joins.sql
@@ -21,3 +21,9 @@ FULL JOIN: Absolutely everything from both sides.  */
 /* SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 FROM Orders
 INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;*/
+
+/* Here we see that the "CustomerID" column in the "Orders" table refers to the "CustomerID" in the "Customers" table. The relationship between the two tables above is the "CustomerID" column.
+Then, we can create the following SQL statement (that contains an INNER JOIN), that selects records that have matching values in both tables:   */
+SELECT orders."OrderID", customers."ContactName", orders."OrderDate"
+FROM orders
+INNER JOIN customers ON orders."CustomerID" = customers."CustomerID";
