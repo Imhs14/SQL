c64ba8a35591d0e948705c515bec364907989cea
imhs14 <heerashanker0214@gmail.com>
2026-06-13 00:40:43 +0530

    updates in tables

---
+++ SQL/23-Inner_join.sql
@@ -0,0 +1,11 @@
+/* SQL INNER JOIN 
+The INNER JOIN  returns only rows that have matching values in both tables. 
+Tip: you can use just JOIN instead of INNER JOIN , as INNER is the default JOIN type. 
+
+INNER JOIN Syntax
+SELECT column_name(s)
+FROM table1
+INNER JOIN table2
+ON table1.column_name = table2.column_name;
+*/
+--- Note : The Syntax combines two tables based on related column, and the ON keyword is used to specify the matching condition. 
